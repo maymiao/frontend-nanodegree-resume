33e5788d39a344f65f1035679dee1eb71e7c66b2
maymiao <maymiao.sz@gmail.com>
2016-07-29 21:00:52 -0400

    Till Dot&Bracket

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,26 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("May M");
+ // $("#main").append("May M");
+
+// var awesomeThoughts="I am May and I am AWESOME";
+// console.log(awesomeThoughts);
+
+// var funthoughts=awesomeThoughts.replace("AWESOME","FUN");
+//  $("#main").append(funthoughts);
+
+// var formattedName=HTMLheaderName.replace("%data%","May");
+// var formattedRole=HTMLheaderRole.replace("%data%","Analyst");
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+var skills="CAS exams"
+var bio={
+"name":"May",
+"role":"Analyst",
+"Contact Info":"guess",
+"Skills":skills
+}
+
+$("#main").append(bio.Skills);
